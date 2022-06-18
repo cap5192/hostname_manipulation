7ddcb58d47fc97e5b99d519247446dba5f647b69
Chintan <cap5192@gmail.com>
2022-06-18 12:42:56 -0500

    test

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ devices = [
      'ssw003.fn1', 'fsw001.vn1', 'ssw002.vn1', 'fsw001.fn1',
     'fsw003.vn1'
 ]
+
 output = {}
 device_type = []
 all_devices = []
@@ -12,9 +13,15 @@ tn1 = []
 vn1 = []
 ssw = []
 fsw = []
-ssw_dictionary = {}
-fsw_dictionary = {}
-nested_dict = []
+ssw_dictionary_fn1 = {}
+fsw_dictionary_fn1 = {}
+ssw_dictionary_tn1 = {}
+fsw_dictionary_tn1 = {}
+ssw_dictionary_vn1 = {}
+fsw_dictionary_vn1 = {}
+nested_dict_fn1 = []
+nested_dict_tn1 = []
+nested_dict_vn1 = []
 
 for i in devices:
     device_type.append(i[0:3])
@@ -43,32 +50,35 @@ for key, value in output.items():
             if x[0:3] == 'fsw':
                 fsw.append(x)
 
-        ssw_dictionary['ssw'] = ssw
-        nested_dict.append(ssw_dictionary)
+        ssw_dictionary_fn1['ssw'] = ssw
+        nested_dict_fn1.append(ssw_dictionary_fn1)
 
-        fsw_dictionary['fsw'] = fsw
-        nested_dict.append(fsw_dictionary)
-        output['fn1'] = nested_dict
+        fsw_dictionary_fn1['fsw'] = fsw
+        nested_dict_fn1.append(fsw_dictionary_fn1)
 
-        nested_dict = []
+
+
+        output['fn1'] = nested_dict_fn1
+
         ssw = []
         fsw = []
 
-    if key == 'tn1':
+
+    elif key == 'tn1':
         for x in value:
             if x[0:3] == 'ssw':
                 ssw.append(x)
             if x[0:3] == 'fsw':
                 fsw.append(x)
 
-        ssw_dictionary['ssw'] = ssw
-        nested_dict.append(ssw_dictionary)
+        ssw_dictionary_tn1['ssw'] = ssw
+        nested_dict_tn1.append(ssw_dictionary_tn1)
 
-        fsw_dictionary['fsw'] = fsw
-        nested_dict.append(fsw_dictionary)
-        output['tn1'] = nested_dict
+        fsw_dictionary_tn1['fsw'] = fsw
+        nested_dict_tn1.append(fsw_dictionary_tn1)
 
-        nested_dict = []
+        output['tn1'] = nested_dict_tn1
+
         ssw = []
         fsw = []
 
@@ -79,27 +89,26 @@ for key, value in output.items():
             if x[0:3] == 'fsw':
                 fsw.append(x)
 
-        ssw_dictionary['ssw'] = ssw
-        nested_dict.append(ssw_dictionary)
+        ssw_dictionary_vn1['ssw'] = ssw
+        nested_dict_vn1.append(ssw_dictionary_vn1)
 
-        fsw_dictionary['fsw'] = fsw
-        nested_dict.append(fsw_dictionary)
-        output['vn1'] = nested_dict
+        fsw_dictionary_vn1['fsw'] = fsw
+        nested_dict_vn1.append(fsw_dictionary_vn1)
+        output['vn1'] = nested_dict_vn1
 
-        nested_dict = []
         ssw = []
         fsw = []
 
-# print(output)
-# site_name = input("Which site do you want to get the devices from?: ")
-# device_choice = input("Which type of device do you want to get?: ")
+
+site_name = input("Which site do you want to get the devices from?: ")
+device_choice = input("Which type of device do you want to get?: ")
 # print(site_name)
 # print(output['fn1'])
-print(output)
-# for i in output[site_name]:
-#     for x,y in i.items():
-#         if x == device_choice:
-#             print(y)
+# print(output)
+for i in output[site_name]:
+    for x,y in i.items():
+        if x == device_choice:
+            print(y)
 
 # test_dict = [{'ssw': ['ssw002.vn1']}, {'fsw': ['fsw001.vn1', 'fsw003.vn1']}]
 # print(test_dict)
